a2d33fb029de0c8da227fda4837adb19a8735731
kellydinneen <kgdinneen@gmail.com>
2021-05-05 12:38:00 -0600

    Set up commander program

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log('hello world');
+var program = require('commander');
+
+program
+  .version('0.0.1')
+
+program
+  .command('convert <source> <destination>')
+  .description('parse access log into JSON and store in new file')
+  .action((source, destination) => {
+     console.log('converting', source);
+     console.log('to', destination);
+   });
+
+program
+  .command('top_agent <log> <date>')
+  .description('query top agent on specified date')
+  .action((log, date) => {
+     console.log('finding top agent in', log);
+     console.log('on', date);
+   });
+
+program
+  .command('top_call <log> <date>')
+  .description('query top method and path on specified date')
+  .action((log, date) => {
+     console.log('finding top call made in', log);
+     console.log('on', date);
+   });
+
+program.parse(process.argv);
