01c51596512423808a690cd6c891831c7b353408
kellydinneen <kgdinneen@gmail.com>
2021-05-06 14:21:23 -0600

    Create pattern for parsing log

--- src/Parser.js
+++ src/Parser.js
@@ -1,12 +1,14 @@
+const Grok = require('./Grok');
+
 class Parser {
   constructor(sourceLog) {
     this.sourceData = sourceLog;
+    this.pattern = '%{IP:client} \\[%{INT:day}/%{MONTH:month}/%{INT:year}:%{TIME:time} -0700\\] "%{WORD:method} %{URIPATHPARAM:url} HTTP/1.1" %{INT:status} %{INT:someNumber} "%{DATA:mtag}" "%{DATA:agent}"';
   }
 
-  convertLog() {
-    //return object matching schema
-  }
+  parseLog() {
 
+  }
 }
 
 module.exports = Parser;
