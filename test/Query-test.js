cf3b539f1a79872d5788296824ddac924173ebff
kellydinneen <kgdinneen@gmail.com>
2021-05-06 22:02:32 -0600

    Write all tests for querying top agent

--- test/Query-test.js
+++ test/Query-test.js
@@ -5,24 +5,44 @@ const Query = require('../src/Query');
 const Parser = require('../src/Parser');
 const logData = require('./dummyData.js');
 
-describe('Query', function() {
+describe.only('Query', function() {
 
-  const query = new Query(logData);
+  const happyQuery = new Query(logData.log);
+  const sadQuery = new Query(logData.log);
 
   it('should be a function', function() {
     expect(Query).to.be.a('function');
   });
 
   it('should be an instance of Query', function() {
-    expect(query).to.be.an.instanceof(Query);
+    expect(happyQuery).to.be.an.instanceof(Query);
   });
 
-  it('should be able to find top agent on given data', function() {
-    expect(query.getTopAgent(date)).to.equal(1);
+  it('should be able to find top agent on given date', function() {
+    expect(happyQuery.getTopAgent(`10/Nov/2020`)).to.deep.equal([
+      {
+        agent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9",
+        hits: 2
+      }
+    ]);
   });
 
-  it('should be able to find top request on given date', function() {
-    expect(query.getTopAgent(date)).to.equal(1);
+  it('should return an error if specified date for top agent query is not included in log', function() {
+    expect(happyQuery.getTopAgent(`10/Nov/2022`)).to.equal('invalid date');
+    expect(happyQuery.getTopAgent(`20/Nov/2020`)).to.equal('invalid date');
+  });
+
+  it('should know if there is a tie between agents', function() {
+    expect(sadQuery.getTopAgent(`10/Nov/2022`)).to.deep.equal([
+      {
+        agent: "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.3",
+        hits: 2
+      },
+      {
+        agent: "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1",
+        hits: 2
+      }
+    ]);
   });
 
 });
